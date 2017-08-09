0d950cc306785bd33792ff8fca9027afe0cb02cc
Benoit Masson-Bedeau <benoit.bedeau@gmail.com>
2017-08-09 20:44:48 +0200

    Added a project to test the Assert class; added details in the Singleton project

--- Singleton/Singleton.cs
+++ Singleton/Singleton.cs
@@ -8,6 +8,9 @@ namespace Singleton
 {
     class Singleton
     {
+        // TODO: no, this isn't the good implementation
+        // see : http://csharpindepth.com/Articles/General/Singleton.aspx
+        // see : https://dzone.com/articles/understanding-and-implementing-singleton-pattern-i
         private static readonly Singleton _Instance;
         public Singleton Instance
         {
---
+++ TestAssert/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAssert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myList = new ArrayList()
+            {
+                1, 2, 3, 4, "toto"
+            };
+
+            foreach (var item in myList)
+            {
+                Trace.Assert(item is int, "L'objet n'est pas un int");
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
