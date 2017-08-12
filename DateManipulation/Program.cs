d64940f7cbc198a1ff4b3f41ff5e5455787d0cc1
Benoit Masson-Bedeau <benoit.masson-bedeau@outlook.com>
2017-08-12 11:49:09 +0200

    Added date manipulation examples

--- DateManipulation/Program.cs
+++ DateManipulation/Program.cs
@@ -40,13 +40,33 @@ namespace DateManipulation
             var dateDEAsDateWithDECulture = DateTime.ParseExact(dateDEAsString, "dd/MM/yyyy", CultureInfo.CreateSpecificCulture("de-DE"));
             var dateDEAsDateWithDECulture2 = DateTime.ParseExact(dateDEAsString, "d", CultureInfo.CreateSpecificCulture("de-DE"));
 
-            // For the output: it doesn't matter as both object has been parsed as date; thei're now date objects and their string representation
+            // For the output: it doesn't matter as both object has been parsed as date; they're now date objects and their string representation
             // are exactly the same
             var invariantToString = dateDEAsDate.ToString();
             var specificToString = dateDEAsDateWithDECulture.ToString();
 
             // The same goes for en english date
             var dateENAsDate = DateTime.ParseExact(dateENAsString, "MM/dd/yyyy", CultureInfo.InvariantCulture);
+
+            // Another example
+            CultureInfo us = new CultureInfo("en-US");
+            CultureInfo sa = new CultureInfo("ar-SA"); // Culture for Saudi Arabia
+
+            string text = "1434-09-23T15:16";
+            string format = "yyyy'-'MM'-'dd'T'HH':'mm";
+
+            // Invariant culture: parses as an english date.
+            // The output is 23 september 1434 if you run this on a environnement with occidental culture
+            // (US, Europe...) because the WriteLine invoke the ToString method of DateTime, which default
+            // to the current culture
+            Console.WriteLine(DateTime.ParseExact(text, format, CultureInfo.InvariantCulture)); // 23 september 1434
+            // same here, because it's in US culture
+            Console.WriteLine(DateTime.ParseExact(text, format, us)); // 23 september 1434
+            // Here, the calendar is not the same! This date will display as 31 july 2013,
+            // in "European" culture!
+            Console.WriteLine(DateTime.ParseExact(text, format, sa));
+            // if we print it in the ar-SA culture however, the result is 23 september 1434
+            Console.WriteLine(DateTime.ParseExact(text, format, sa).ToString(format, sa));
 
             Console.ReadLine();
         }
