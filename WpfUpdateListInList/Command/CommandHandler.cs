6069f7c9a9900a47163367531f14876e191c7a22
Benoit Masson-Bedeau <benoit.bedeau@gmail.com>
2017-09-14 18:15:45 +0200

    Added a project to test ObservableCollection

---
+++ WpfUpdateListInList/Command/CommandHandler.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WpfUpdateListInList.Command
+{
+    /// <summary>
+    /// Non-generic class that implements ICommand. This class takes a action without parameter that
+    /// can be executed if a condition is met.
+    /// </summary>
+    public class CommandHandler : ICommand
+    {
+        /// <summary>Action to execute in case the command is executable</summary>
+        private Action MethodToExecute = null;
+
+        /// <summary>Method to determine whether the command is executable or not</summary>
+        private Func<bool> MethodToDetermineCanExecute = null;
+
+        /// <summary>
+        /// Parameterized constructor
+        /// </summary>
+        /// <param name="methodToExecute">Method to execute in case the command is executable</param>
+        /// <param name="methodToDetermineCanExecute">Method to determine whether the command is executable or not</param>
+        public CommandHandler(Action methodToExecute, Func<bool> methodToDetermineCanExecute)
+        {
+            this.MethodToExecute = methodToExecute;
+            this.MethodToDetermineCanExecute = methodToDetermineCanExecute;
+        }
+
+        /// <summary>
+        /// Determine whether the command is executable or not
+        /// </summary>
+        /// <param name="parameter">Optional parameters</param>
+        /// <returns>True if the method can be executed, false otherwise</returns>
+        public bool CanExecute(object parameter)
+        {
+            if (MethodToDetermineCanExecute == null)
+            {
+                return true;
+            }
+            else
+            {
+                return MethodToDetermineCanExecute();
+            }
+        }
+
+        /// <summary>
+        /// Execute the command
+        /// </summary>
+        /// <param name="parameter">optional parameters</param>
+        public void Execute(object parameter)
+        {
+            MethodToExecute();
+        }
+
+        /// <summary>
+        /// Event handler to raise when the method to determine whether the command
+        /// is executable or not changed
+        /// </summary>
+        public event EventHandler CanExecuteChanged;
+
+        /// <summary>
+        /// Raise the event indicating that something changed and we need
+        /// to check if the command is now executable or not
+        /// </summary>
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ WpfUpdateListInList/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using WpfUpdateListInList.Command;
+
+namespace WpfUpdateListInList.ViewModel
+{
+    public class MyItem : INotifyPropertyChanged
+    {
+        // The class MUST implement INotifyPropertyChanged in order
+        // to notify that an item has changed in an ObservableCollection
+
+        private string _Color;
+        public string Color
+        {
+            get { return this._Color; }
+            set
+            {
+                SetProperty(null, ref this._Color, value);
+            }
+        }
+
+        public MyItem(string color)
+        {
+            Color = color;
+        }
+
+        #region event handling
+        /// <summary>Event handler</summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Raising the changed property event
+        /// </summary>
+        /// <param name="propertyName">Changed property</param>
+        protected void RaisedPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            if (this.PropertyChanged != null)
+            {
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        /// <summary>
+        /// Field change notification only if the field has really changed
+        /// </summary>
+        /// <typeparam name="T">Field type</typeparam>
+        /// <param name="checkCommands">Action that check the commands everytime a property changes</param>
+        /// <param name="storage">Initial value</param>
+        /// <param name="value">Updated value</param>
+        /// <param name="propertyName">Property name</param>
+        /// <returns>true if the field value changed, false otherwise</returns>
+        protected bool SetProperty<T>(Action checkCommands, ref T storage, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (Equals(storage, value))
+            {
+                return false;
+            }
+            else
+            {
+                storage = value;
+                RaisedPropertyChanged(propertyName);
+                checkCommands?.Invoke();
+                return true;
+            }
+        }
+        #endregion event handling
+    }
+
+    class MainWindowViewModel : ViewModelBase
+    {
+        public ObservableCollection<MyItem> MyItems { get; set; }
+
+        public CommandHandler MakeFirstRedCommand { get; set; }
+
+        public MainWindowViewModel()
+        {
+            MakeFirstRedCommand = new CommandHandler(MakeFirstRedClick, () => true);
+
+            MyItems = new ObservableCollection<MyItem>();
+            MyItems.Add(new MyItem("blue"));
+            MyItems.Add(new MyItem("white"));
+            MyItems.Add(new MyItem("red"));
+        }
+
+        public void MakeFirstRedClick()
+        {
+            MyItems[0].Color = "red";
+        }
+
+        public void CheckCommands()
+        {
+            MakeFirstRedCommand.RaiseCanExecuteChanged();
+        }
+    }
+}
