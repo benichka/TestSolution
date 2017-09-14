4f3e3abe208c087d009acf0ee18773d930174764
Benoit Masson-Bedeau <benoit.masson-bedeau@outlook.com>
2017-09-14 17:12:29 +0200

    Updated the WPF project and added a simple currency converter project that uses a simple MVVM implementation

---
+++ WpfCurrencyConverter/DataModel/Currency.cs
@@ -0,0 +1,31 @@
+﻿namespace WpfCurrencyConverter.DataModel
+{
+    /// <summary>
+    /// Class describing a currency
+    /// </summary>
+    public class Currency
+    {
+        public string Title { get; set; }
+        public decimal Rate { get; set; }
+
+        /// <summary>
+        /// Build a currency based on a title and a change rate against euro
+        /// </summary>
+        /// <param name="title">Currency title (euro, dollar...)</param>
+        /// <param name="rate">Exchange rate against euro</param>
+        public Currency(string title, decimal rate)
+        {
+            Title = title;
+            Rate = rate;
+        }
+
+        /// <summary>
+        /// Represent the currency as a string
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return Title;
+        }
+    }
+}
---
+++ WpfCurrencyConverter/ViewModel/ViewModelBase.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfCurrencyConverter.ViewModel
+{
+    /// <summary>
+    /// Base class for the view models.<para />
+    /// This class mainly handles the property changed interface.
+    /// </summary>
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        #region event handling
+        /// <summary>Event handler</summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Raising the changed property event
+        /// </summary>
+        /// <param name="propertyName">Changed property</param>
+        protected void RaisedPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            if (this.PropertyChanged != null)
+            {
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        /// <summary>
+        /// Field change notification only if the field has really changed
+        /// </summary>
+        /// <typeparam name="T">Field type</typeparam>
+        /// <param name="checkCommands">Action that check the commands everytime a property changes</param>
+        /// <param name="storage">Initial value</param>
+        /// <param name="value">Updated value</param>
+        /// <param name="propertyName">Property name</param>
+        /// <returns>true if the field value changed, false otherwise</returns>
+        protected bool SetProperty<T>(Action checkCommands, ref T storage, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (Equals(storage, value))
+            {
+                return false;
+            }
+            else
+            {
+                storage = value;
+                RaisedPropertyChanged(propertyName);
+                checkCommands?.Invoke();
+                return true;
+            }
+        }
+        #endregion event handling
+    }
+}
